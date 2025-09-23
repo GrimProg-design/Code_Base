de6c5992c0474d29e8f452d4617ac4ca5e34353b
GrimProg <vavilovila01@gmail.com>
2025-09-23 15:35:10 +0600

    Add profile block

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { lazy, Suspense } from 'react'
+
+const Profile = lazy(() => import("./components/profile/Profile"))
 
 function App() {
   return (
     <main className='main-wrapper'>
-
+      <Suspense fallback={<div>Loading...</div>}>
+        <Profile/>
+      </Suspense>
     </main>
   )
 }
