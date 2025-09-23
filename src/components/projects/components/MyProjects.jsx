d1fd9ff02e6f26c2175130ff0abb02b4cc523d15
GrimProg <vavilovila01@gmail.com>
2025-09-23 23:48:49 +0600

    Add my projects

--- src/components/projects/components/MyProjects.jsx
+++ src/components/projects/components/MyProjects.jsx
@@ -1,8 +1,80 @@
 export default function MyProjects() {
-    return (
-        <div className="project-wrappers">
-            <img src="" alt="" />
-            <a href="">Храм Покрова</a>
-        </div>
-    )
+  return (
+    <div className="project-wrappers">
+      <ul>
+        <li>
+          <img src="" alt="" />
+          <a
+            href="https://grimprog-design.github.io/for-church/?page=home"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Храм Покрова
+          </a>
+        </li>
+
+        <li>
+          <img src="" alt="" />
+          <a
+            href="https://grimprog-design.github.io/Life_timer/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Live Timer
+          </a>
+        </li>
+
+        <li>
+            <a
+            href="https://grimprog-design.github.io/gift-for-you/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gift-For-Sofi
+          </a>
+        </li>
+
+        <li>
+            <a
+            href=" https://grimprog-design.github.io/cake-for-sofi/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cake-For-Sofi
+          </a>
+        </li>
+
+        <li>
+          <img src="" alt="" />
+          <a
+            href="https://grimprog-design.github.io/my-tic-tac-toe/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My-Tic-Tac-Toe
+          </a>
+        </li>
+
+        <li>
+          <img src="" alt="" />
+          <a
+            href="https://www.npmjs.com/package/path-easy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Path_Easy
+          </a>
+        </li>
+
+        <li>
+          <img src="" alt="" />
+          <a
+            href="https://grimprog-design.github.io/css-authorization/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >CSS-Authorization</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
