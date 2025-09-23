da07ccb04a0449528fa2e6efaa1ee03df7ee215e
GrimProg <vavilovila01@gmail.com>
2025-09-23 16:06:31 +0600

    Add info box

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './App.css'
 import { lazy, Suspense } from 'react'
 
 const Profile = lazy(() => import("./components/profile/Profile"));
+const Contacts = lazy(() => import("./components/contacts/Contacts"));
 const Header = lazy(() => import("./components/header/Header"));
-
+const Info = lazy(() => import("./components/info/Info"))
 
 function App() {
   return (
@@ -14,6 +15,14 @@ function App() {
 
       <Suspense fallback={<div>Loading...</div>}>
         <Header/>
+      </Suspense>
+
+      <Suspense>
+        <Contacts/>
+      </Suspense>
+
+      <Suspense>
+        <Info/>
       </Suspense>
     </main>
   )
