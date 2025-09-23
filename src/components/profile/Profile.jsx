2a08cb619d4cadc7f217b82c0e2ce2e39c0b95c7
GrimProg <vavilovila01@gmail.com>
2025-09-23 17:20:53 +0600

    Made profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,9 +1,13 @@
 import "./Profile.css"
+import Avatar from "./avatar/Avatar"
 
 export default function Profile(){
     return(
         <div className="boxes-wrapper profile-wrapper">
-            
+            <Avatar
+                alt="Фото Вавилова Ильи"
+                caption="Вавилов Илья"
+            />
         </div>
     )
 }
--- src/components/profile/avatar/Avatar.jsx
+++ src/components/profile/avatar/Avatar.jsx
@@ -1,3 +1,16 @@
-export default function Avatar() {
-    
+import AvatarPhoto from "../../../assets/profile-assets/avatar-photo.jpg";
+
+export default function Avatar({ alt, caption }) {
+  return (
+    <div className="wrapper-for-me">
+      <figure className="photo-wrapper">
+        <img src={AvatarPhoto} alt={alt} className="photo" />
+      </figure>
+      <div className="figurecaption-wrapper">
+        <figcaption className="photo-caption">{caption}</figcaption>
+        <p>19 лет</p>
+      </div>
+      <p>FullStack developer</p>
+    </div>
+  );
 }
