a31910efd3b483217a3f00d928e20268c6454f6c
GrimProg <vavilovila01@gmail.com>
2025-09-25 21:29:02 +0600

    Add switch

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,7 +1,16 @@
+import "./Settings.css"
+
+import Switch from "./components/Switch"
+
 export default function Settings() {
     return (
         <div className="boxes-wrapper settings-wrapper">
-
+            <p>Settings</p>
+            <div className="switch-wrapper">
+                <Switch/>
+                <Switch/>
+                <Switch/>
+            </div>
         </div>
     )
 }
---
+++ src/components/settings/components/DarkMode.jsx
@@ -0,0 +1,8 @@
+export default function DarkMode() {
+    return (
+        <label class="switch">
+            <input type="checkbox"/>
+            <span class="slider round"></span>
+        </label>
+    )
+}
