27931c75a896c7c3c627ba74c6048e78e8c82486
GrimProg <vavilovila01@gmail.com>
2025-09-28 21:08:30 +0600

    Добавил шапку сайта

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,15 @@
 import "./Header.css"
 
 export default function Header() {
-    return (
-        <div className="boxes-wrapper header-wrapper">
-            <h1>Code_Base</h1>
-        </div>
-    )
+  return (
+    <div className="boxes-wrapper header-wrapper">
+      <h1>Code_Base</h1>
+      <p className="header-description">
+        Личный веб-портал, где собрана вся информация обо мне как разработчике.
+        Здесь можно найти:  
+        <strong> проекты, навыки, опыт, контакты</strong> и краткая информация обо мне.
+        Ознакомься с моими работами, изучи технологии, которыми я владею, и свяжись со мной напрямую.
+      </p>
+    </div>
+  )
 }
